8436267bfd11d11c2101d1d9dc19b076bde92752
Toya <66713857+NorauDn@users.noreply.github.com>
2022-06-16 13:47:35 +0900

    a

--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -12,10 +12,54 @@ namespace fukuvHensu
 {
     public partial class Form1 : Form
     {
+        int x, y;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += (int.Parse(textBox1.Text));
+            label1.Top += (int.Parse(textBox2.Text));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("堀 朱莉");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            y =+(int.Parse(textBox1.Text));
+            textBox2.Text = "0";
+            textBox1.Text = "5";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            x =-(int.Parse(textBox2.Text));
+            textBox2.Text = "5";
+            textBox1.Text = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            y =-(int.Parse(textBox1.Text));
+            textBox2.Text = "0";
+            textBox1.Text = "-5";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x =+(int.Parse(textBox2.Text));
+            textBox2.Text = "-5";
+            textBox1.Text = "0";
+        }
     }
 }
